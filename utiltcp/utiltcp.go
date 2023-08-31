a9b26ff1f39b9908ee9f8183da2aef2b8bb6fdaa
shreevatshan <83694809+shreevatshan@users.noreply.github.com>
2023-08-31 12:49:24 +0530

    read till delim

--- utiltcp/utiltcp.go
+++ utiltcp/utiltcp.go
@@ -1,24 +1,24 @@
 package utiltcp
 
 import (
+	"bufio"
 	"net"
 	"time"
 )
 
 const (
 	PROTOCOL = "tcp"
-	READSIZE = 1024
 	TIMEOUT  = 5
 	TYPE_UNI = "uni"
 	TYPE_BI  = "bi"
 )
 
 type Request struct {
-	Timeout  int
-	Readsize int64
-	Address  string
-	Type     string
-	Body     []byte
+	Timeout int
+	Address string
+	Type    string
+	Delim   byte
+	Body    []byte
 }
 
 type Response struct {
@@ -45,23 +45,17 @@ func (req *Request) Send() Response {
 
 	if req.Type != TYPE_UNI {
 
-		if req.Readsize == 0 {
-			req.Readsize = READSIZE
-		}
 		if req.Timeout == 0 {
 			req.Timeout = TIMEOUT
 		}
 		conn.SetReadDeadline(time.Now().Add(time.Duration(req.Timeout) * time.Second))
 
-		buf := make([]byte, req.Readsize)
-
-		_, err = conn.Read(buf)
+		buf, err := bufio.NewReader(conn).ReadBytes(req.Delim)
 		if err != nil {
 			res.Err = err
 			conn.Close()
 			return res
 		}
-
 		res.Body = buf
 	}
 	conn.Close()
